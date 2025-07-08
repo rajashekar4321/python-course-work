240a364f88714c804fa1a8203df9fe874a03bb34
rajashekar4321 <rajashekarshathakoti@gmail.com>
2025-07-08 08:36:34 +0530

    second

---
+++ if.py
@@ -0,0 +1,11 @@
+email,pwd=input("enter the email and pwd:").split()
+if data.get(email)==pwd:
+    print('login successful')
+else:
+    print('invalid login')
+
+
+a="hello world"
+print(a)
+
+   
