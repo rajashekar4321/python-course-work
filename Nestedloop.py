1ee1c0827adefc6eee0489692a472ce57d6bb830
rajashekar4321 <rajashekarshathakoti@gmail.com>
2025-07-17 08:56:22 +0530

    program

--- Nestedloop.py
+++ Nestedloop.py
@@ -31,19 +31,54 @@ for i in range(1, n + 1):
 
 
 
-
-
-n=int(input())
-n=5
-for i in range(n):
-   for j in range(n):
-      if i==3 or i==4 or j==3:
-       print('*',end=' ')
-   print()
-
-n=int(input("Enter the number:"))
+n=7
 for row in range(n):
    for col in range (n):
       if col==0 or row==0 or row==n-1 or col==n-1:
-         print('*',end='  ')
+         print('*',end=' ')
+      else:
+         print(' ',end=' ')
    print()
+
+
+#A
+n=7
+for row in range (n):
+   for col in range (n):
+      if col==0 or row==0 or col==n-1 or row==n//2:
+          print('*',end=' ')
+      else:
+         print(' ',end=' ')
+   print()   
+
+
+#B
+n=5
+for row in range (n):
+   for col in range(n):
+      if col==0 or row==0 or col==n-1 or row==n-1 or row==n//2:
+          print('*',end=' ')
+      else:
+         print(' ',end=' ')
+   print()
+
+#c
+for row in range (n):
+   for col in range(n):
+      if col==0 or row==0 or row==n-1:
+          print('*',end=' ')
+      else:
+         print(' ',end=' ')
+   print()
+
+for i in range (0,3):
+   for j in range (0,3):
+    if (j<=i):
+      print(j+1,end=" ")
+   print()
+
+
+   
+
+
+
